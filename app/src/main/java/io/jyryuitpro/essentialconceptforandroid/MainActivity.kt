88d8a1619634b552190ab1ff16bf140231719351
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-04-10 22:51:44 +0900

    42. ViewControl - 개념

--- app/src/main/java/io/jyryuitpro/essentialconceptforandroid/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/essentialconceptforandroid/MainActivity.kt
@@ -10,34 +10,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("LifeCycle", "onCreate")
     }
-
-    override fun onStart() {
-        Log.d("LifeCycle", "onStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d("LifeCycle", "onResume")
-        super.onResume()
-    }
-
-    override fun onPause() {
-        Log.d("LifeCycle", "onPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d("LifeCycle", "onStop")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d("LifeCycle", "onDestroy")
-        super.onDestroy()
-    }
-
-    override fun onRestart() {
-        Log.d("LifeCycle", "onRestart")
-        super.onRestart()
-    }
 }
