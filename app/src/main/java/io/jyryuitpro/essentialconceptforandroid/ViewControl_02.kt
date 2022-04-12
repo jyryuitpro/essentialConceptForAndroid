c6db5647ae23442bb6ba337fd802c78b97084932
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-04-12 23:39:30 +0900

    44. ViewControl - 실습 2편

--- app/src/main/java/io/jyryuitpro/essentialconceptforandroid/ViewControl_02.kt
+++ app/src/main/java/io/jyryuitpro/essentialconceptforandroid/ViewControl_02.kt
@@ -3,6 +3,7 @@ package io.jyryuitpro.essentialconceptforandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,9 +12,30 @@ class ViewControl_02 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_control02)
 
+        // 뷰를 코틀린 파일(Activity)로 가져오는 방법
         val textViewOne: TextView = findViewById(R.id.textViewOne)
         val buttonOne: Button = findViewById(R.id.buttonOne)
-
         Log.d("testt", textViewOne.text.toString())
+
+        // 풀버전
+        val clickListener = object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d("testt", "버튼 클릭!!")
+            }
+        }
+        buttonOne.setOnClickListener(clickListener)
+
+        // 축약 버전 1 (익명함수)
+        buttonOne.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d("testt", "버튼 클릭!!")
+            }
+        })
+
+        // Listener 사용 방법, 람다버전
+        buttonOne.setOnClickListener {
+            // 버튼이 클릭되었을 때, 동작할 코드
+            Log.d("testt", "버튼 클릭!!")
+        }
     }
 }
