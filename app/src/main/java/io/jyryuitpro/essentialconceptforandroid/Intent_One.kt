d11959d1c385799f008f26443dcae6c94c67d9ae
jyr <jyr@wavers.co.kr>
2022-04-16 12:07:43 +0900

    52. Intent - 실습 1편

---
+++ app/src/main/java/io/jyryuitpro/essentialconceptforandroid/Intent_One.kt
@@ -0,0 +1,30 @@
+package io.jyryuitpro.essentialconceptforandroid
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Intent_One : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_one)
+
+        // 암시적 인텐트
+        // - 전화, SMS, Google Play Store, Website, Google Map, 사진첩 등등
+        // - 상수
+        //      - 변하지 않는 수(문자)
+        //      - 상수의 변수명은 전부 대문자로 만드는 문화가 있다
+        // - URI (Uniform Resource Identifier)
+        //      - id라고 생각을 하면 된다 -> 고유하다
+        //      - 자원을 나타내는 고유한 주소
+        // - URL
+        //      - 인터넷 페이지의 고유한 주소
+        val implicit_intent: TextView = findViewById(R.id.implicit_intent)
+        implicit_intent.setOnClickListener {
+            val intent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "010-9378-9025"))
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/io/jyryuitpro/essentialconceptforandroid/Intent_Two.kt
@@ -0,0 +1,11 @@
+package io.jyryuitpro.essentialconceptforandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Intent_Two : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_two)
+    }
+}
