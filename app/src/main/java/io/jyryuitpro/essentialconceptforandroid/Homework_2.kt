a030c2996e993be96e0269cf0723f21943bc9bd3
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-04-17 11:38:31 +0900

    65. 과제 - 풀이 2편

--- app/src/main/java/io/jyryuitpro/essentialconceptforandroid/Homework_2.kt
+++ app/src/main/java/io/jyryuitpro/essentialconceptforandroid/Homework_2.kt
@@ -5,14 +5,25 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.webkit.WebView
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doBeforeTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class Homework_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homework2)
 
+        val webview = findViewById<WebView>(R.id.webview)
+        val urlPrefix = "https://"
+        var finalUrl = ""
+
         val address = findViewById<EditText>(R.id.address)
+
+        // addTextChangedListener 사용법
         address.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 Log.d("testt", "beforeTextChanged : " + s)
@@ -23,8 +34,21 @@ class Homework_2 : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
+                finalUrl = urlPrefix + s.toString()
                 Log.d("testt", "afterTextChanged : " + s)
             }
         })
+
+        // addTextChangedListener 사용법 -> 람다 형식
+        address.doBeforeTextChanged { text, start, count, after ->  }
+        address.doOnTextChanged { text, start, before, count ->  }
+        address.doAfterTextChanged {  }
+
+        val open = findViewById<TextView>(R.id.open)
+        open.setOnClickListener {
+            val url = address.text.toString()
+            Log.d("testt", url)
+            webview.loadUrl(finalUrl)
+        }
     }
 }
